3bda7f3e4d9874f839624b808809e13a9b7383f4
Igdálias Cabamba <igdaliasmussulveiracabamba@gmail.com>
2022-09-19 23:47:56 +0100

    added root endpoint

--- server/main.py
+++ server/main.py
@@ -1,9 +1,13 @@
-from fastapi import FastAPI
 import src.config as _set
 _set.up()
 
+from fastapi import FastAPI, responses
 from src.routes import api_app
 
 app = FastAPI()
 
 app.mount(path = "/api", app = api_app, name = "API")
+
+@app.get("/")
+def index():
+    return responses.RedirectResponse("/api")
